6ad4301851658b133111ca2baefa61530c951ece
Walter Berggren <walter.berggren@reaktor.com>
2022-08-23 08:00:17 -0500

    Use Compose 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val compose_version by extra("1.2.0-rc03")
+    val compose_version by extra("1.2.0")
     val compose_compiler_version by extra("1.2.0")
 
     repositories {
@@ -8,7 +8,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("com.android.tools.build:gradle:7.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
 
         // NOTE: Do not place your application dependencies here; they belong
